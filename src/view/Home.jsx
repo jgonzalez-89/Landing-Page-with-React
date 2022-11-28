ff0dbd8a9e486aa40e3d25e30e02d67e3620feb3
jgonzalez-89 <kbcerverjo@gmail.com>
2022-11-28 03:32:49 +0100

    fix import Home

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Jumbotron from '../components/Jumbotron';
 import Card from '../components/Card';
-import Footer from '../components/Footer';
+import Footer from '../components/footer';
 
 const Home = () => {
 	return (
