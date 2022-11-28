67cfda0e8d2305ea2cd3ad077694f1c95d92cc85
Jose L. Gonzalez Diaz <kbcerverjo@gmail.com>
2022-11-28 01:32:03 +0000

    fixed router

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import Navbar from '../components/navbar';
-import Jumbotron from '../components/jumbotron';
-import Card from '../components/card';
-import Footer from "../components/Footer";
+import Navbar from "/workspace/Landing-Page-with-React/src/components/navbar.jsx";
+import Jumbotron from "/workspace/Landing-Page-with-React/src/components/jumbotron.jsx";
+import Card from "/workspace/Landing-Page-with-React/src/components/card.jsx";
+import Footer from "/workspace/Landing-Page-with-React/src/components/footer.jsx";
+
 
 const Home = () => {
 	return (
-        <div className="text-center">
-            <Navbar />
-            <Jumbotron />
+        <div class="text-center">
+            <Navbar/>
+            <Jumbotron/>
             <div class="row g-4 p-5 mb-4 row-cols-1 row-cols-md-2 row-cols-lg-3  row-cols-xl-4">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
             </div>
-        <Footer />
+        <Footer/>
         </div>
 	);
 };
